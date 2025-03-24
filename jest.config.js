60c75bf1277a78b5f93d8a665eb691a29c7f3c37
hemalatha67970 <hemalatha.manikandan6797@gmail.com>
2025-03-24 19:30:31 +0530

    modified

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: 'jsdom',  // Set the test environment to jsdom
+    testEnvironmentOptions: {
+      url: 'http://localhost:3000/',  // Set the URL to localhost:3000
+    },
+    // Other Jest configurations...
+  };
+  
