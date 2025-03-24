5b9495991ce78f7b3e74fb115f4233ecf8f62314
hemalatha67970 <hemalatha.manikandan6797@gmail.com>
2025-03-24 20:12:39 +0530

    test

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
     testEnvironmentOptions: {
       url: 'http://localhost:3000/',  // Set the URL to localhost:3000
     },
+    watchPlugins: [
+      "jest-watch-typeahead/filename",  // Plugin to filter by filename
+      "jest-watch-typeahead/testname"   // Plugin to filter by test name
+    ],
     // Other Jest configurations...
   };
   
